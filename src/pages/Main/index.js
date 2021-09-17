8a2304d66841ce5e08e6b43289449114f5268d6c
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-16 22:59:09 -0300

    alteração no styled component do main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import {Container, Form, SubmitButton} from './styles'
 import {FaGithub, FaPlus} from 'react-icons/fa'
 
 export default function Main(){
+  
+  const [newRepo, setNewRepo] = useState('')
+  
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    console.log(newRepo)
+  }
+
+  
+  function handleInputChange(e) {
+    setNewRepo(e.target.value)
+  }
+
+  
   return(
     <Container>
       <h1>
@@ -10,8 +27,13 @@ export default function Main(){
         Meus Repositorios
       </h1>
 
-      <Form onSubmit={()=>{}}>
-        <input type="text" placeholder="Adicionar repositorios" />
+      <Form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Adicionar repositorios" 
+        value={newRepo}
+        onChange={handleInputChange}
+        />
       
         <SubmitButton>
           <FaPlus color="#FFF" size={14} />
