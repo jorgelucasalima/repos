eae0364827b1ae557b5d9979aea6ec33ec17038a
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-17 16:40:42 -0300

    realizado alteração

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './styles';
 import {FaArrowCircleLeft} from 'react-icons/fa'
 import api from '../../services/api'
 
@@ -8,6 +8,7 @@ export default function Repositorio({match}){
   const [repositorio, setRepositorio] = useState({})
   const [issues, setIssues] = useState([])
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
 
 
   useEffect(() => {
@@ -36,6 +37,34 @@ export default function Repositorio({match}){
     
   }, [match.params.repositorio]);
   
+
+
+  useEffect(() => {
+    
+    async function loadIssue(params) {
+      const nomeRepo = decodeURIComponent(match.params.repositorio)
+    
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params:{
+          state: 'open',
+          page,
+          per_page: 5,
+        },
+      })
+
+      setIssues(response.data)
+    
+    }
+
+    loadIssue()
+
+  }, [match.params.repositorio ,page]);
+
+
+  function handlePage(action) {
+    setPage(action === 'back' ? page - 1 : page + 1 )
+  }
+
 
   if (loading) {
     return(
@@ -80,6 +109,21 @@ export default function Repositorio({match}){
           </li>
         ))}
       </IssuesList>
+
+      <PageActions>
+        <button 
+        type="button" 
+        onClick={()=> handlePage('back')}
+        disabled={page < 2}
+        >
+          Voltar
+        </button>
+
+        <button type="button" onClick={()=> handlePage('next')}>
+          Proxima
+        </button>          
+      </PageActions>
+
 
     </Container>
   )
