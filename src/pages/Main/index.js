c1236736d2d2df5ff3060565bb543948ae9aa573
jorgelucasalima <jorgelucasalima@gmail.com>
2021-09-17 11:42:13 -0300

    criado useeffect para salvar e buscar os repositorio no json

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from "react";
+import React, {useState, useCallback, useEffect} from "react";
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles'
 import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa'
 import api from '../../services/api'
@@ -11,6 +11,25 @@ export default function Main(){
   const [newRepo, setNewRepo] = useState('')
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  //Buscar
+  useEffect(() => {
+    const repoStorage = localStorage.getItem('repos')
+
+    if (repoStorage) {
+      setRepositorios(JSON.parse(repoStorage))
+    }
+
+  }, []);
+
+
+
+  //Salvar Alterações
+  useEffect(() => {
+    localStorage.setItem('repos', JSON.stringify(repositorios))
+  }, [repositorios]);
+
 
 
   const handleSubmit = useCallback((e) => {    
@@ -18,9 +37,23 @@ export default function Main(){
 
     async function submit() {
       setLoading(true)
+      setAlert(null)
       try{
+
+        if (newRepo === '') {
+          throw new Error('Indique um repositorio')
+        }
+
+
         const response = await api.get(`repos/${newRepo}`)
     
+        const hasRepo = repositorios.find(repo => repo.name === newRepo)
+
+        if (hasRepo) {
+          throw new Error('Repositorio Duplicado')
+        }
+
+
         const data = {
           name: response.data.full_name,
         }  
@@ -29,6 +62,7 @@ export default function Main(){
         setNewRepo('')
 
       }catch(error){
+        setAlert(true)
         console.log(error)
       }finally{
         setLoading(false)
@@ -44,6 +78,7 @@ export default function Main(){
   
   function handleInputChange(e) {
     setNewRepo(e.target.value)
+    setAlert(null)
   }
 
 
@@ -60,7 +95,7 @@ export default function Main(){
         Meus Repositorios
       </h1>
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error={alert}>
         <input 
         type="text" 
         placeholder="Adicionar repositorios" 
